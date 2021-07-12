95156166f54dff0878e6aea6041f6338e25fe98a
Cole Michaels <cmichaelsd@gmail.com>
2021-07-12 09:56:12 +0900

    first commit

---
+++ app/src/main/java/com/colemichaels/masterdetailflowdemo/WebsiteDetailHostActivity.kt
@@ -0,0 +1,34 @@
+package com.colemichaels.masterdetailflowdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.colemichaels.masterdetailflowdemo.databinding.ActivityWebsiteDetailBinding
+
+class WebsiteDetailHostActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityWebsiteDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_website_detail) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_website_detail)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
